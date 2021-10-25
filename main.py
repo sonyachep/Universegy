42f1eb397bdfc04bc8831558e2a2d2157cf2dc97
sonyachep <70879898+sonyachep@users.noreply.github.com>
2021-10-25 18:56:42 +0700

    add git

---
+++ main.py
@@ -0,0 +1,26 @@
+import sys
+
+from PyQt5 import uic  # Импортируем uic
+from PyQt5.QtWidgets import QApplication, QMainWindow
+
+
+class Universegy(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        uic.loadUi('universegy.ui', self)
+        self.stackedWidget.setCurrentIndex(0)
+        self.login.clicked.connect(self.RunToPage2)
+        self.enterance_btn.clicked.connect(self.log_in)
+
+    def RunToPage2(self):
+        self.stackedWidget.setCurrentIndex(1)
+
+    def log_in(self):
+        pass
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    ex = Universegy()
+    ex.show()
+    sys.exit(app.exec_())
